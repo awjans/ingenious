da2f5850de472f8d88c22f5447dcd460db1415a9
awjans <andreww@jans.org>
2019-12-20 16:57:03 -1000

    intial code base

---
+++ php/public/index.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>Ingenious Partners</title>
+    </head>
+    <body>
+        <header>
+            <h1>Ingenious Partners</h1>
+        </header>
+        <div>
+            <h2>Welcome</h2>
+            <p>Greetings and salutations from our family to yours.</p>
+        </div>
+        <footer>
+            <p>Copyright &copy; 2020 <a href="//www.ingenious.partners/">Ingenious Partners</a> - All Rights Reserved</p>
+        </footer>
+    </body>
+</html>
---
+++ refresh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd "${0%/*}"
+pwd
+git pull
+find -type d -exec chmod -R 755 {} \;
+find -type f -exec chmod -R 644 {} \;
+chown -R root:www-data php
+chmod 750 refresh.sh
+cp apache2/ingenious.partners.conf /etc/apache2/sites-available/ingenious.partners.conf
