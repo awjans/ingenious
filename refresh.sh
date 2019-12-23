ec74c51a5fd9daaa3a0df26939bf6ff6a6ee1203
root <root@ingenious.partners>
2019-12-23 00:47:37 +0000

    fixing redirect of http to https for mail subdomain

--- refresh.sh
+++ refresh.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd "${0%/*}"
-pwd
 git pull
 find -type d -exec chmod -R 755 {} \;
 find -type f -exec chmod -R 644 {} \;
 chown -R root:www-data php
-chmod 750 refresh.sh
+chmod ug+x refresh.sh
 cp apache2/ingenious.partners.conf /etc/apache2/sites-available/ingenious.partners.conf
