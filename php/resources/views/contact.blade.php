bfb514ab4d1e6cf3191f0ad327c479ef3be796a2
awjans <andreww@jans.org>
2020-05-25 18:00:21 -1000

    update

--- php/resources/views/contact.blade.php
+++ php/resources/views/contact.blade.php
@@ -6,6 +6,5 @@
 
 @section('content')
 <h2>Contact Information</h2>
-<p>Mililani, Hawaii<br><a href="tel:+18088249534">+1.808.824.9534</a><br><a href="mailto:info@ingenious.partners">info@ingenious.partners</a></p>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d282572.3891862288!2d-158.0967996298951!3d21.479482593259604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c006704b74dc5fd%3A0xb68b50a4e1d2a215!2sMililani%2C%20HI%2096789!5e0!3m2!1sen!2sus!4v1578198459747!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+<p>Ewa Beach, Hawaii<br><a href="tel:+18088249534">+1.808.824.9534</a><br><a href="mailto:info@ingenious.partners">info@ingenious.partners</a></p>
 @endsection
