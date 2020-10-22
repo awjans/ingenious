48eee87670079b34d687fa114da7970adc5e6cd6
awjans <57602919+awjans@users.noreply.github.com>
2020-10-22 11:35:24 -1000

    added post-refresh script

--- refresh.sh
+++ refresh.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd "${0%/*}"
 git pull
-find -type d -exec chmod 755 {} \;
-find -type f -exec chmod 644 {} \;
-chown -R root:www-data php
-chmod ug+x refresh.sh
-cp apache2/ingenious.partners.conf /etc/apache2/sites-available/ingenious.partners.conf
+chmod ug+x refresh.sh post-refresh.sh
+./post-refresh.sh
