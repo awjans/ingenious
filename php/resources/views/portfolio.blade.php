61cca1d7b2a3c33f30db231ce54bc6a3e30428f8
awjans <57602919+awjans@users.noreply.github.com>
2020-10-28 14:21:21 -1000

    Fixed with of quotes

--- php/resources/views/portfolio.blade.php
+++ php/resources/views/portfolio.blade.php
@@ -11,19 +11,19 @@
 <h1>Portfolio</h1>
 <h2>Hawaii Select Investments Inc</h2>
 <p>Stategic Advice, Technical Support</p>
-<blockquote class="testimonial col-3 col-md-6">
+<blockquote class="testimonial col-9">
 <p>I can always count on Ingenious Partners to give me straight answers on how I can leverage the right technology to make my business run better. They don't try to sell me the latest trend unless it actually applies to me. Ingenious Partners is there for me when I need them.</p>
 <footer>&dash;<cite>Bob Dewitz, CEO</cite></footer>
 </blockquote>
 <h2><a href="https://www.cranetech.pro" target="ipclient">Crane Tech LLC</a></h2>
 <p>Techical Support, Systems Administration</p>
-<blockquote class="testimonial col-3 col-md-6">
+<blockquote class="testimonial col-9">
 <p>Ingenious Partners provide fast reliable services and offer many creative solutions for our small business needs. Hands down one of the best ala-cart I.T. service solutions available on Oahu.</p>
 <footer>&dash;<cite>General Manager</cite></footer>
 </blockquote>
 <h2><a href="https://www.generatorpowersys.com" target="ipclient">Generator & Power Systems LLC</a></h2>
 <p>Technical Support, Systems Administration</p>
-<blockquote class="testimonial col-3 col-md-6">
+<blockquote class="testimonial col-9">
 <p>Ingenious Partners provided us with a robust customized solution that met our growing needs and helped us maintain budget. With the pandemic in full swing, they have been able to continue to serve our I.T. needs whether it is remote log ins or always available phone support.</p>
 <footer>&dash;<cite>General Manager</cite></footer>
 </blockquote>
