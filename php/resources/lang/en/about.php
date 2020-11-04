534823e283e12ac293a04a3f8cc937925c0602c5
awjans <57602919+awjans@users.noreply.github.com>
2020-11-03 14:31:21 -1000

    even more localization

---
+++ php/resources/lang/en/about.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+	'description' => 'At Ingenious Partners we believe that technology should be a help, not a hinderence. We know you are busy and dealing with IT issues can be time-consuming and confusing. Let our experts ease those frustrations for you. We will step in to provide any level of support you may need.',
+	'companyHistory' => '<h1>Company History</h1>
+<p>In the fall of 2019, ...</p>
+',
+	'person' => [
+		'kathy' => [
+			'alt' => 'Kathy Jans',
+			'text' => 'Bla, blah, blah...',
+		],
+		'andrea' => [
+			'alt' => 'Andrea Jans',
+			'text' => 'Bla, blah, blah...',
+		],
+	],
+];
--- php/resources/views/about.blade.php
+++ php/resources/views/about.blade.php
@@ -5,7 +5,17 @@
 @endsection
 
 @section('description')
+@lang('about.description')
 @endsection
 
 @section('content')
+<div class='row'>@lang('about.companyHistory')</div>
+<div class='row'>
+<div class='float-left'><img src="{{ asset('images/about-kathy.jpg') }}" alt="@lang('about.person.kathy.alt')"></div>
+<div>@lang('about.person.kathy.text')</div>
+</div>
+<div class='row'>
+<div class='float-left'><img src="{{ asset('images/about-andrea.jpg') }}" alt="@lang('about.person.andrea.alt')"></div>
+<div>@lang('about.person.andrea.text')</div>
+</div>
 @endsection
