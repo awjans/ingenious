19510dc0f2bbc899a880742db1f809cd5952d614
awjans <57602919+awjans@users.noreply.github.com>
2020-11-05 14:28:03 -1000

    more localization

--- php/resources/lang/en/about.php
+++ php/resources/lang/en/about.php
@@ -4,16 +4,16 @@ return [
 
 	'description' => 'At Ingenious Partners we believe that technology should be a help, not a hinderence. We know you are busy and dealing with IT issues can be time-consuming and confusing. Let our experts ease those frustrations for you. We will step in to provide any level of support you may need.',
 	'companyHistory' => '<h1>Company History</h1>
-<p>In the fall of 2019, ...</p>
+<p>In the spring of 2020, Kathy and Andrea Jans started Ingenious Partners, LLC with the goal of helping small companies leverage technology. We believed not just big companies should be able to take advantage of computers, cloud computing and web communication. The goal is to bring affordable Technical Support, System Administration, Strategic Advice, Custom Software and Web Design to even the smallest of companies.</p>
 ',
 	'person' => [
 		'kathy' => [
 			'alt' => 'Kathy Jans',
-			'text' => 'Bla, blah, blah...',
+			'text' => 'Blah, blah, blah...',
 		],
 		'andrea' => [
 			'alt' => 'Andrea Jans',
-			'text' => 'Bla, blah, blah...',
+			'text' => 'Blah, blah, blah...',
 		],
 	],
 ];
--- php/resources/views/contact.blade.php
+++ php/resources/views/contact.blade.php
@@ -4,7 +4,9 @@
 @lang('website.contact')
 @endsection
 
+@section('description')
+@lang('contact.description')
+@endsection
+
 @section('content')
-<h2>Contact Information</h2>
-<p>Ewa Beach, Hawaii<br>Phone:&nbsp;<a href="tel:+18088249534">+1.808.824.9534</a><br />Email:&nbsp;<a href="mailto:info@ingenious.partners">info@ingenious.partners</a></p>
 @endsection
