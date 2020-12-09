9980db392d549d978533ddc88a48ad6f8f24c153
awjans <andreww@jans.org>
2020-12-08 18:23:49 -1000

    adding contact us form

--- php/resources/views/contact.blade.php
+++ php/resources/views/contact.blade.php
@@ -9,4 +9,5 @@
 @endsection
 
 @section('content')
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSerjbne7Og627Sb2-0WC3HkhauY7t80T5tyMwUqjCsPqWHGyQ/viewform?embedded=true" width="640" height="1055" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 @endsection
